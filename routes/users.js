7d2be27ebf602ff899b659b7802ecc029caca269
Kalash Mishra <mishrakalash18@gmail.com>
2023-11-16 14:44:05 +0530

    change

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var plm = require('passport-local-mongoose');
-const { post } = require('.');
+const { post } = require('./post');
 mongoose.connect("mongodb://127.0.0.1:27017/passport");
 var userSchema = mongoose.Schema({
     username: String,
